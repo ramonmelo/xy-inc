851cd00fb659b228a93d7a30c3febfd5877dc2b8
Ramon Melo <me@ramonmelo.com>
2016-07-28 20:30:50 -0300

    create form request to create a new POI, using ajax

--- static/js/create.js
+++ static/js/create.js
@@ -1,8 +1,32 @@
 $(function() {
 
+    alert = $("#alert_box");
+
     form = $("#create_form");
 
-    form.on('submit', function() {
+    form.on('submit', function(evt) {
+        evt.preventDefault();
 
+        out_data = $(this).serialize();
+
+        $.post('/create_poi/', out_data, function(data, status) {
+            alert.removeClass('alert-warning alert-info');
+
+            if (status == 'success') {
+                alert.addClass(data['error'] ? 'alert-warning' : 'alert-info');
+                alert.find("#alert_content").html(data['msg']);
+
+                if (!data['error']) {
+                    form.trigger("reset");
+                }
+
+            } else {
+                alert.addClass('alert-warning');
+                alert.find("#alert_content").html('An error occurred during the request. Please try again.');
+            }
+
+            alert.toggleClass('hidden show');
+            setTimeout(function() { alert.toggleClass('hidden show') }, 5000);
+        });
     });
 });
