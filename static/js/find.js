157f1865266b070e689eb087e761744b898d34ce
Ramon Melo <me@ramonmelo.com>
2016-07-28 23:10:18 -0300

    add methods for get data

--- static/js/find.js
+++ static/js/find.js
@@ -1,3 +1,36 @@
 $(function() {
 
+    form = $("#find_form");
+    table = $("#result_table");
+
+    row_template = $("<tr></tr>");
+
+    var update = function() {
+        table.empty();
+
+        out_data = $(form).serialize();
+
+        $.get('/list_poi/', out_data, function(data, status) {
+            if (!data['error']) {
+                for (var i = 0; i < data['result'].length; i++) {
+                    obj = data['result'][i];
+
+                    row = row_template.clone();
+
+                    row.append( $("<td></td>").html(obj['name']) );
+                    row.append( $("<td></td>").html(obj['x']) );
+                    row.append( $("<td></td>").html(obj['y']) );
+
+                    table.append(row);
+                }
+            }
+        });
+    };
+
+    form.on('submit', function(evt) {
+        evt.preventDefault();
+        update();
+    });
+
+    update();
 });
